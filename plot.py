438de029328adbbc09371d1903e62f11a3909060
Andreas <andreas.lefdalsnes@outlook.com>
2016-10-06 17:36:43 +0200

    Clean plot.py

--- plot.py
+++ plot.py
@@ -14,37 +14,9 @@ with open("positions.dat", "r") as infile:
 	for i in range(1, numberOfPlanets+1):
 		planets[i] = data[i+1::numberOfPlanets+2]
 
-	# a = np.asarray(planets[1][0].split()).astype(float)
-	# print a
-
 	r_earth = np.zeros((n,4))
 	for i in range(n-1):
 		r_earth[i] = np.asarray(planets[2][i].split()).astype(float)
-
-	# x_earth = []
-	# for i in range(n):
-	# 	x_earth.append(float(planets[2][i][0]))
-
-	# print x_earth[0]
-	# print type(x_earth[0])
-
-
-	# for i in range(1, numberOfPlanets+1):
-	# 	for j in range(n):
-	# 		print planets[i][j][2]
-
-	# sun = data[2::numberOfPlanets+2]
-	# earth = data[3::numberOfPlanets+2]
-	
-	# # t = np.linspace()
-	# x = r[:,0]
-	# y = r[:,1]
-	# z = r[:,2]
-
-	# x_earth = x[1::2]
-	# # print x_earth
-	# y_earth = y[1::2]
-	# z_earth = z[1::2]
 
 	mpl.rcParams['legend.fontsize'] = 10
 	fig = plt.figure()
