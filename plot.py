4cef967d94e4c72732cdb2fd1fbde7862bdbef49
Tellef Storebakken <tellefstorebakken@gmail.com>
2016-10-06 18:14:35 +0200

    verlet method works. tried to make a python function to plot all planets, no luck

--- plot.py
+++ plot.py
@@ -3,7 +3,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 with open("positions.dat", "r") as infile:
 
 	data = np.char.strip(np.array(infile.readlines()))
@@ -16,17 +15,18 @@ with open("positions.dat", "r") as infile:
 
 	# a = np.asarray(planets[1][0].split()).astype(float)
 	# print a
-
+	
+	
 	r_earth = np.zeros((n,4))
 	for i in range(n-1):
 		r_earth[i] = np.asarray(planets[2][i].split()).astype(float)
 
-	r_sun = np.zeros((n,4))
-	for i in range(n-1):
-		r_sun[i] = np.asarray(planets[1][i].split()).astype(float)
+	def extract_plot_planet(integer_number):
+		r_planet = np.zeros((n,4))
+		for i in range(n-1):
+			r_planet[i] = np.asarray(planets[integer_number][i].split()).astype(float)
+		ax.plot(r_planet[0:-1,1],r_planet[0:-1,2],r_planet[0:-1,3], label="balle")
 	
-	
-
 	# x_earth = []
 	# for i in range(n):
 	# 	x_earth.append(float(planets[2][i][0]))
@@ -56,6 +56,8 @@ with open("positions.dat", "r") as infile:
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
 	ax.plot(r_earth[0:-1,1],r_earth[0:-1,2],r_earth[0:-1,3], label="balle")
+	for k in range(3):
+		extract_plot_planet(k)
 	ax.legend()
 
 	plt.show()
