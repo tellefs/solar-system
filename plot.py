5d38209b83b181511a400c72b7ce0843be90c89b
Andreas <andreas.lefdalsnes@outlook.com>
2016-10-19 00:59:57 +0200

    Minor clean-up, try to implement mercury energy

--- plot.py
+++ plot.py
@@ -2,32 +2,43 @@ import numpy as np
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import os
 
-with open("../build-SolarSystem-Desktop_Qt_5_7_0_clang_64bit-Debug/positions.dat", "r") as infile:
-
+def read_planets(file):
 	data = np.char.strip(np.array(infile.readlines()))
+	
 	numberOfPlanets = int(data[0])
 	n = len(data)/(numberOfPlanets+2)
-
 	planets = {}
 	for i in range(1, numberOfPlanets+1):
 		planets[i] = data[i+1::numberOfPlanets+2]
+	return numberOfPlanets, n, planets
 
-	def extract_plot_planet(integer_number):
-		r_planet = np.zeros((n,4))
-		for i in range(n-1):
-			r_planet[i] = np.asarray(planets[integer_number][i].split()).astype(float)
-		ax.plot(r_planet[0:-1,1],r_planet[0:-1,2],r_planet[0:-1,3], label="balle")
+def extract_plot_planet(planet_number, numberOfPlanets, n, planets, figure):
+	r_planet = np.zeros((n,4))
+	for i in range(n-1):
+		r_planet[i] = np.asarray(planets[planet_number][i].split()).astype(float)
+	figure.plot(r_planet[0:-1,1],r_planet[0:-1,2],r_planet[0:-1,3], label="balle")
 
+def plot_solarsystem(file):
 	mpl.rcParams['legend.fontsize'] = 10
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
+
+	numberOfPlanets, n, planets = read_planets(file)
 	for k in range(1, numberOfPlanets+1):
-		extract_plot_planet(k)
+		extract_plot_planet(k, numberOfPlanets, n, planets, ax)
 	ax.legend()
 	ax.set_xlim3d(-2,2)
 	ax.set_ylim3d(-2,2)
 	ax.set_zlim3d(-2,2)
 
+
+with open("../build-SolarSystem-Desktop_Qt_5_7_0_clang_64bit-Debug/positions.dat", "r") as infile:
+
+	plot_solarsystem(infile)
+	plt.show()
+
+with open("../build-SolarSystem-Desktop_Qt_5_7_0_clang_64bit-Debug/mercury_system.dat", "r") as infile:
+
+	plot_solarsystem(infile)
 	plt.show()
